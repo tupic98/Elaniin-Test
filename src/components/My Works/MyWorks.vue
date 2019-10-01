a02f88eb33ac04203c8d41f9773fc6a7db2b8455
Rodrigo Alvarenga <00017316@uca.edu.sv>
2019-10-01 02:42:06 -0600

    Creating 'My works' component

---
+++ src/components/My Works/MyWorks.vue
@@ -0,0 +1,166 @@
+<template>
+  <div id="works" class="my-works">
+    <div class="top__left"></div>
+    <div class="top__center-left">
+      <v-layout column align-center justify-center fill-height>
+        <div
+          class="white--text"
+        >
+          <p class="works-text">My Works.</p>
+          <v-divider class="white line"></v-divider>
+          <p class="works-paragraph">A photo - is a search for what can get into the frame. When you limit</p>
+          <p class="works-paragraph">events to a frame - You change these events</p>
+        </div>
+      </v-layout>
+    </div>
+    <div class="top__center-right"></div>
+    <div class="top__right"></div>
+    <div class="bottom">
+      <div class="img1">
+        <v-img
+          class="image1"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          height="305"
+          width="395"
+        >
+        </v-img>
+        <v-layout column fill-height align-start class="white--text info1">
+          <div class="text-uppercase grey--text info_title">Make</div>
+          <p class="info_desc">Canon</p>
+          <div class="text-uppercase grey--text info_title">Model</div>
+          <p class="info_desc">Canon EOS 5D Mark II</p>
+          <div class="text-uppercase grey--text info_title">Shutter speed</div>
+          <p class="info_desc">1/4000s</p>
+          <div class="text-uppercase grey--text info_title">Aperture</div>
+          <p class="info_desc">f/4.5</p>
+          <div class="text-uppercase grey--text info_title">Focal lenght</div>
+          <p class="info_desc">230mm</p>
+          <div class="text-uppercase grey--text info_title">iso</div>
+          <p class="info_desc">250</p>
+        </v-layout>
+      </div>
+      <div class="img2">
+        <v-img
+          class="image2"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          height="305"
+          width="395"
+        >
+        </v-img>
+        <v-layout column fill-height align-start class="white--text info2">
+          <div class="text-uppercase grey--text info_title">Make</div>
+          <p class="info_desc">Canon</p>
+          <div class="text-uppercase grey--text info_title">Model</div>
+          <p class="info_desc">Canon EOS 5D Mark II</p>
+          <div class="text-uppercase grey--text info_title">Shutter speed</div>
+          <p class="info_desc">1/4000s</p>
+          <div class="text-uppercase grey--text info_title">Aperture</div>
+          <p class="info_desc">f/4.5</p>
+          <div class="text-uppercase grey--text info_title">Focal lenght</div>
+          <p class="info_desc">230mm</p>
+          <div class="text-uppercase grey--text info_title">iso</div>
+          <p class="info_desc">250</p>
+        </v-layout>
+      </div>
+      <div class="img3">
+        <v-img
+          class="image3"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          height="612"
+          width="395"
+        >
+        </v-img>
+        <v-layout column fill-height align-start class="white--text info3">
+          <div class="text-uppercase grey--text info_title">Make</div>
+          <p class="info_desc">Canon</p>
+          <div class="text-uppercase grey--text info_title">Model</div>
+          <p class="info_desc">Canon EOS 5D Mark II</p>
+          <div class="text-uppercase grey--text info_title">Shutter speed</div>
+          <p class="info_desc">1/4000s</p>
+          <div class="text-uppercase grey--text info_title">Aperture</div>
+          <p class="info_desc">f/4.5</p>
+          <div class="text-uppercase grey--text info_title">Focal lenght</div>
+          <p class="info_desc">230mm</p>
+          <div class="text-uppercase grey--text info_title">iso</div>
+          <p class="info_desc">250</p>
+        </v-layout>
+      </div>
+      <div class="img4">
+        <v-img
+          class="image4"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          height="305"
+          width="791"
+        >
+        </v-img>
+        <v-layout column fill-height align-start class="white--text info4">
+          <div class="text-uppercase grey--text info_title">Make</div>
+          <p class="info_desc">Canon</p>
+          <div class="text-uppercase grey--text info_title">Model</div>
+          <p class="info_desc">Canon EOS 5D Mark II</p>
+          <div class="text-uppercase grey--text info_title">Shutter speed</div>
+          <p class="info_desc">1/4000s</p>
+          <div class="text-uppercase grey--text info_title">Aperture</div>
+          <p class="info_desc">f/4.5</p>
+          <div class="text-uppercase grey--text info_title">Focal lenght</div>
+          <p class="info_desc">230mm</p>
+          <div class="text-uppercase grey--text info_title">iso</div>
+          <p class="info_desc">250</p>
+        </v-layout>
+      </div>
+      <div class="img5">
+        <v-img
+          class="image5"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          height="305"
+          width="395"
+        >
+        </v-img>
+        <v-layout column fill-height align-start class="white--text info5">
+          <div class="text-uppercase grey--text info_title">Make</div>
+          <p class="info_desc">Canon</p>
+          <div class="text-uppercase grey--text info_title">Model</div>
+          <p class="info_desc">Canon EOS 5D Mark II</p>
+          <div class="text-uppercase grey--text info_title">Shutter speed</div>
+          <p class="info_desc">1/4000s</p>
+          <div class="text-uppercase grey--text info_title">Aperture</div>
+          <p class="info_desc">f/4.5</p>
+          <div class="text-uppercase grey--text info_title">Focal lenght</div>
+          <p class="info_desc">230mm</p>
+          <div class="text-uppercase grey--text info_title">iso</div>
+          <p class="info_desc">250</p>
+        </v-layout>
+      </div>
+      <div class="img6">
+        <v-img
+          class="image6"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          height="305"
+          width="395"
+        >
+        </v-img>
+        <v-layout column fill-height align-start class="white--text info6">
+          <div class="text-uppercase grey--text info_title">Make</div>
+          <p class="info_desc">Canon</p>
+          <div class="text-uppercase grey--text info_title">Model</div>
+          <p class="info_desc">Canon EOS 5D Mark II</p>
+          <div class="text-uppercase grey--text info_title">Shutter speed</div>
+          <p class="info_desc">1/4000s</p>
+          <div class="text-uppercase grey--text info_title">Aperture</div>
+          <p class="info_desc">f/4.5</p>
+          <div class="text-uppercase grey--text info_title">Focal lenght</div>
+          <p class="info_desc">230mm</p>
+          <div class="text-uppercase grey--text info_title">iso</div>
+          <p class="info_desc">250</p>
+        </v-layout>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" src="./_myWorks.ts">
+</script>
+
+<style lang="scss" scoped>
+  @import "_myWorks.scss";
+</style>

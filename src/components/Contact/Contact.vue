99b09b777e9020bf487d9eed0bc9aa9a40450419
Rodrigo Alvarenga <00017316@uca.edu.sv>
2019-10-01 02:39:57 -0600

    Created Contact component

---
+++ src/components/Contact/Contact.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fluid class="contact-footer white--text" id="contact">
+    <v-layout row justify-center align-center fill-height>
+      <v-layout column justify-center align-center>
+        <p class="contact-paragraph">Sing up for our newsletter</p>
+        <p class="contact-paragraph">to receive special offers.</p>
+        <div class="vertical-line"></div>
+        <v-text-field style="width: 400px;" dark outlined placeholder="Enter your email address"></v-text-field>
+      </v-layout>
+    </v-layout>
+  </v-container>
+</template>
+<script lang="ts" src="./_contact.ts">
+</script>
+
+<style lang="scss" scoped>
+  @import "_contact.scss";
+</style>
---
+++ src/components/Contact/_contact.ts
@@ -0,0 +1,6 @@
+import { Component, Vue } from 'vue-property-decorator';
+// @ts-ignore
+
+@Component
+export default class Contact extends Vue {
+}

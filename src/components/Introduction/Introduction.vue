5c2b0f106315e18a16bd769bae9937b2ebfc397b
Rodrigo Alvarenga <00017316@uca.edu.sv>
2019-10-01 02:40:24 -0600

    Creating Introduction component

--- src/components/Introduction/Introduction.vue
+++ src/components/Introduction/Introduction.vue
@@ -1,144 +1,66 @@
 <template>
-  <v-container>
-    <v-layout
-      text-center
-      wrap
+  <div id="home" class="introduction">
+    <div
+      class="left_side"
     >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
+      <v-layout class="sides" column align-start justify-center fill-height>
+        <ul class="sides__elements">
+          <a href="#">
+            <li class="icon text-center">Instagram</li>
           </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
+          <a href="#">
+            <li class="icon text-center">Twitter</li>
           </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
+          <a href="#">
+            <li class="icon text-center">Facebook</li>
           </a>
-        </v-layout>
-      </v-flex>
-    </v-layout>
-  </v-container>
+        </ul>
+      </v-layout>
+    </div>
+    <div
+      class="center_left"
+    >
+      <v-layout column align-center justify-center fill-height>
+        <div
+          class="white--text"
+        >
+          <p class="text-uppercase">Introduction</p>
+          <p class="main__text" v-html="mainText"></p>
+          <v-divider class="white line"></v-divider>
+          <p class="main__paragraph text-justify" v-html="mainParagraph"></p>
+
+          <p>Discover</p>
+        </div>
+      </v-layout>
+    </div>
+    <div
+      class="center_right"
+    >
+    </div>
+    <div
+      class="right_side"
+    >
+      <v-layout class="sides" column align-start justify-center fill-height>
+        <ul class="sides__elements_right">
+          <a href="#">
+            <li class="icon" id="explore">
+              <div
+                class="explore__text"
+              >
+                <v-divider class="white explore__text_line"></v-divider>
+                EXPLORE MORE
+              </div>
+            </li>
+          </a>
+        </ul>
+      </v-layout>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-
-export default Vue.extend({
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
-});
+<script lang="ts" src="./_introduction.ts">
 </script>
+
+<style scoped>
+  @import "_introduction.scss";
+</style>

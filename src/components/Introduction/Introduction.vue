ccdaf82f135fda0fc9f0639b3af0267f6b870bf9
Rodrigo Alvarenga <00017316@uca.edu.sv>
2019-10-01 11:43:25 -0600

    Added background to introduction component

--- src/components/Introduction/Introduction.vue
+++ src/components/Introduction/Introduction.vue
@@ -1,61 +1,66 @@
 <template>
-  <div id="home" class="introduction">
-    <div
-      class="left_side"
-    >
-      <v-layout class="sides" column align-start justify-center fill-height>
-        <ul class="sides__elements">
-          <a href="#">
-            <li class="icon text-center">Instagram</li>
-          </a>
-          <a href="#">
-            <li class="icon text-center">Twitter</li>
-          </a>
-          <a href="#">
-            <li class="icon text-center">Facebook</li>
-          </a>
-        </ul>
-      </v-layout>
-    </div>
-    <div
-      class="center_left"
-    >
-      <v-layout column align-center justify-center fill-height>
-        <div
-          class="white--text"
-        >
-          <p class="text-uppercase">Introduction</p>
-          <p class="main__text" v-html="mainText"></p>
-          <v-divider class="white line"></v-divider>
-          <p class="main__paragraph text-justify" v-html="mainParagraph"></p>
+  <v-parallax
+    height="730"
+    :src="banner"
+  >
+    <div id="home" class="introduction">
+      <div
+        class="left_side"
+      >
+        <v-layout class="sides" column align-start justify-center fill-height>
+          <ul class="sides__elements">
+            <a href="#">
+              <li class="icon text-center">Instagram</li>
+            </a>
+            <a href="#">
+              <li class="icon text-center">Twitter</li>
+            </a>
+            <a href="#">
+              <li class="icon text-center">Facebook</li>
+            </a>
+          </ul>
+        </v-layout>
+      </div>
+      <div
+        class="center_left"
+      >
+        <v-layout column align-center justify-center fill-height>
+          <div
+            class="white--text"
+          >
+            <p class="text-uppercase">Introduction</p>
+            <p class="main__text" v-html="mainText"></p>
+            <v-divider class="white line"></v-divider>
+            <p class="main__paragraph text-justify" v-html="mainParagraph"></p>
 
-          <p>Discover</p>
-        </div>
-      </v-layout>
+            <p>Discover</p>
+          </div>
+        </v-layout>
+      </div>
+      <div
+        class="center_right"
+      >
+      </div>
+      <div
+        class="right_side"
+      >
+        <v-layout class="sides" column align-start justify-center fill-height>
+          <ul class="sides__elements_right">
+            <a href="#">
+              <li class="icon" id="explore">
+                <div
+                  class="explore__text"
+                >
+                  <v-divider class="white explore__text_line"></v-divider>
+                  EXPLORE MORE
+                </div>
+              </li>
+            </a>
+          </ul>
+        </v-layout>
+      </div>
     </div>
-    <div
-      class="center_right"
-    >
-    </div>
-    <div
-      class="right_side"
-    >
-      <v-layout class="sides" column align-start justify-center fill-height>
-        <ul class="sides__elements_right">
-          <a href="#">
-            <li class="icon" id="explore">
-              <div
-                class="explore__text"
-              >
-                <v-divider class="white explore__text_line"></v-divider>
-                EXPLORE MORE
-              </div>
-            </li>
-          </a>
-        </ul>
-      </v-layout>
-    </div>
-  </div>
+  </v-parallax>
 </template>
 
 <script lang="ts" src="./_introduction.ts">
--- src/components/Introduction/_introduction.ts
+++ src/components/Introduction/_introduction.ts
@@ -1,12 +1,16 @@
 import { Component, Vue } from 'vue-property-decorator';
+// @ts-ignore
+import banner from '@/assets/banner.jpg';
 
 @Component
 export default class Introduction extends Vue {
+  banner: any = banner;
+
   mainText: string = 'Photography is \n a journey.'
 
-  mainParagraph: string = 'My advice to everyone is to create life, not consume it. I\'m curious to \n ' +
-    'see how many emotions now appear in things created with the help of \n ' +
-    'complex technologies. They are so loaded with different feelings that it \n ' +
-    'is no longer possible to separate emotions and work from the \n ' +
-    ' instrument on which is done.'
+  mainParagraph: string = 'My advice to everyone is to create life, not consume it. I\'m curious to \n '
+    + 'see how many emotions now appear in things created with the help of \n '
+    + 'complex technologies. They are so loaded with different feelings that it \n '
+    + 'is no longer possible to separate emotions and work from the \n '
+    + ' instrument on which is done.'
 }

c56383dc1cd9e58543971a74296816926435487e
Rodrigo Alvarenga <00017316@uca.edu.sv>
2019-10-01 11:46:24 -0600

    Changed Works from grid to flex

--- src/components/My Works/_myWorks.ts
+++ src/components/My Works/_myWorks.ts
@@ -2,4 +2,8 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class MyWorks extends Vue {
+  lorem: string = 'Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio '
+    + 'vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. '
+    + 'Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. '
+    + 'Pri eu justo aeque torquatos.'
 }
